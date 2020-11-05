4a9071894259960ccbee41c98efbffb328854cfd
vimal <vimalku637@gmail.com>
2020-11-05 13:37:43 +0530

    initial commit

---
+++ app/src/main/java/spartons/com/imagecropper/MainActivity.java
@@ -0,0 +1,229 @@
+package spartons.com.imagecropper;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.yalantis.ucrop.UCrop;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import spartons.com.imagecropper.enums.ImagePickerEnum;
+import spartons.com.imagecropper.listeners.IImagePickerLister;
+import spartons.com.imagecropper.utils.FileUtils;
+import spartons.com.imagecropper.utils.UiHelper;
+
+public class MainActivity extends AppCompatActivity implements IImagePickerLister {
+
+    private static final int CAMERA_ACTION_PICK_REQUEST_CODE = 610;
+    private static final int PICK_IMAGE_GALLERY_REQUEST_CODE = 609;
+    public static final int CAMERA_STORAGE_REQUEST_CODE = 611;
+    public static final int ONLY_CAMERA_REQUEST_CODE = 612;
+    public static final int ONLY_STORAGE_REQUEST_CODE = 613;
+
+    private String currentPhotoPath = "";
+    private UiHelper uiHelper = new UiHelper();
+    private ImageView imageView;
+    private String base64="";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.selectPictureButton).setOnClickListener(v -> {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                if (uiHelper.checkSelfPermissions(this))
+                    uiHelper.showImagePickerDialog(this, this);
+        });
+        imageView = findViewById(R.id.imageView);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_STORAGE_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED)
+                uiHelper.showImagePickerDialog(this, this);
+            else if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_DENIED) {
+                uiHelper.toast(this, "ImageCropper needs Storage access in order to store your profile picture.");
+                finish();
+            } else if (grantResults[0] == PackageManager.PERMISSION_DENIED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                uiHelper.toast(this, "ImageCropper needs Camera access in order to take profile picture.");
+                finish();
+            } else if (grantResults[0] == PackageManager.PERMISSION_DENIED && grantResults[1] == PackageManager.PERMISSION_DENIED) {
+                uiHelper.toast(this, "ImageCropper needs Camera and Storage access in order to take profile picture.");
+                finish();
+            }
+        } else if (requestCode == ONLY_CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                uiHelper.showImagePickerDialog(this, this);
+            else {
+                uiHelper.toast(this, "ImageCropper needs Camera access in order to take profile picture.");
+                finish();
+            }
+        } else if (requestCode == ONLY_STORAGE_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                uiHelper.showImagePickerDialog(this, this);
+            else {
+                uiHelper.toast(this, "ImageCropper needs Storage access in order to store your profile picture.");
+                finish();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_ACTION_PICK_REQUEST_CODE && resultCode == RESULT_OK) {
+            Uri uri = Uri.parse(currentPhotoPath);
+            openCropActivity(uri, uri);
+        } else if (requestCode == UCrop.REQUEST_CROP && resultCode == RESULT_OK) {
+            if (data != null) {
+                Uri uri = UCrop.getOutput(data);
+                showImage(uri);
+            }
+        } else if (requestCode == PICK_IMAGE_GALLERY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            try {
+                Uri sourceUri = data.getData();
+                File file = getImageFile();
+                Uri destinationUri = Uri.fromFile(file);
+                openCropActivity(sourceUri, destinationUri);
+            } catch (Exception e) {
+                uiHelper.toast(this, "Please select another image");
+            }
+        }
+    }
+
+    private void openImagesDocument() {
+        Intent pictureIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        pictureIntent.setType("image/*");
+        pictureIntent.addCategory(Intent.CATEGORY_OPENABLE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            String[] mimeTypes = new String[]{"image/jpeg", "image/png"};
+            pictureIntent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+        }
+        startActivityForResult(Intent.createChooser(pictureIntent, "Select Picture"), PICK_IMAGE_GALLERY_REQUEST_CODE);
+    }
+
+    private void showImage(Uri imageUri) {
+        try {
+            File file;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+                file = FileUtils.getFile(this, imageUri);
+            } else {
+                file = new File(currentPhotoPath);
+            }
+            InputStream inputStream = new FileInputStream(file);
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            imageView.setImageBitmap(bitmap); //set to show image
+
+            bitmap = getResizedBitmap(bitmap, 300); //get resize image
+            base64 = getStringImage(bitmap); //get base 64 image file.
+            Log.e("image", base64);
+        } catch (Exception e) {
+            uiHelper.toast(this, "Please select different profile picture.");
+        }
+    }
+
+    @Override
+    public void onOptionSelected(ImagePickerEnum imagePickerEnum) {
+        if (imagePickerEnum == ImagePickerEnum.FROM_CAMERA)
+            openCamera();
+        else if (imagePickerEnum == ImagePickerEnum.FROM_GALLERY)
+            openImagesDocument();
+    }
+
+    private void openCamera() {
+        Intent pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File file;
+        try {
+            file = getImageFile(); // 1
+        } catch (Exception e) {
+            e.printStackTrace();
+            uiHelper.toast(this, "Please take another image");
+            return;
+        }
+        Uri uri;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) // 2
+            uri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID.concat(".provider"), file);
+        else
+            uri = Uri.fromFile(file); // 3
+        pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri); // 4
+        startActivityForResult(pictureIntent, CAMERA_ACTION_PICK_REQUEST_CODE);
+    }
+
+    private File getImageFile() throws IOException {
+        String imageFileName = "JPEG_" + System.currentTimeMillis() + "_";
+        File storageDir = new File(
+                Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_DCIM
+                ), "Camera"
+        );
+        System.out.println(storageDir.getAbsolutePath());
+        if (storageDir.exists())
+            System.out.println("File exists");
+        else
+            System.out.println("File not exists");
+        File file = File.createTempFile(
+                imageFileName, ".jpg", storageDir
+        );
+        currentPhotoPath = "file:" + file.getAbsolutePath();
+        return file;
+    }
+
+    private void openCropActivity(Uri sourceUri, Uri destinationUri) {
+        UCrop.Options options = new UCrop.Options();
+        options.setCircleDimmedLayer(true);
+        options.setCropFrameColor(ContextCompat.getColor(this, R.color.colorAccent));
+        UCrop.of(sourceUri, destinationUri)
+                .withMaxResultSize(100, 100)
+                .withAspectRatio(5f, 5f)
+                .start(this);
+    }
+
+    public static String getStringImage(Bitmap image) {
+        ByteArrayOutputStream byteArrayOS = null;
+        try {
+            System.gc();
+            byteArrayOS = new ByteArrayOutputStream();
+            image.compress(Bitmap.CompressFormat.JPEG, 20, byteArrayOS);
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+            image.compress(Bitmap.CompressFormat.JPEG, 20, byteArrayOS);
+        }
+        return Base64.encodeToString(byteArrayOS.toByteArray(), Base64.DEFAULT);
+    }
+
+    private Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float) width / (float) height;
+        if (bitmapRatio > 0) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return Bitmap.createScaledBitmap(image, width, height, true);
+    }
+}
---
+++ app/src/main/java/spartons/com/imagecropper/enums/ImagePickerEnum.java
@@ -0,0 +1,11 @@
+package spartons.com.imagecropper.enums;
+
+/**
+ * Ahsen Saeed}
+ * ahsansaeed067@gmail.com}
+ * 2/6/19}
+ */
+public enum ImagePickerEnum {
+    FROM_GALLERY,
+    FROM_CAMERA
+}
